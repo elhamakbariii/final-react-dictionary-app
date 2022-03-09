134db6f44e2c8ce5483797e054cf5b4be3e6f3a3
elhamakbariii <94735414+elhamakbariii@users.noreply.github.com>
2022-03-09 15:47:36 +0330

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.png'
+import Dictionary from './Dictionary'
+import Result from './Result'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="Container">
+        <header className="App-header">
+        <img src={logo} className="logo" alt="logo" />
+        <br/>
+           <h1><strong> Dictionary</strong></h1> 
+          
+        </header>
+        <Dictionary defaultKeyword="amazing" />
+        <Result defaultKeyword="amazing" />
+        <footer>
+          coded by<em> Elham Akbari</em> , open source code on{' '}
+          <a
+            href="https://github.com/elhamakbariii/final-react-dictionary-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+             <strong>github</strong>
+          </a>
+        </footer>
+      </div>
+    </div>
+  )
+}
