dc47f1f53db8e444aee41d6cb6b18dbec17b0586
elhamakbariii <94735414+elhamakbariii@users.noreply.github.com>
2022-03-14 16:28:38 +0330

    change

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,27 +1,23 @@
+import React from "react";
+import Synonyms from "./Synonyms"
 
-import React from 'react'
-import Synonyms from './Synonyms'
-import Antonyms from './Antonyms'
-import './Meaning.css'
-
-export default function Meaning(props) {
-  if (props.meaning) {
+export default function Meaning(props){
+    console.log(props);
     return (
-      <div className="Meaning">
-        <h3>{props.meaning.partOfSpeech}</h3>
-        {props.meaning.definitions.map(function (definition, index) {
-          return (
-            <div key={index}>
-              <p>--● {definition.definition}</p>
-              <em>{definition.example}</em>
-              <Synonyms synonyms={definition.synonyms} />
-              <Antonyms antonyms={definition.antonyms} />
-            </div>
-          )
-        })}
-      </div>
+        <div className="Meaning">
+            <h3>{props.meaning.partOfSpeech}</h3>
+            {props.meaning.definitions.map(function(definition, index){
+                return (
+                  <div key={index}>
+                      <p className="mt-3 mb-4">
+                          <strong>Definition: </strong>
+                          {definition.definition}
+                      </p>
+                      <em className="example">{definition.example}</em>
+                      <Synonyms synonyms={definition.synonyms}/>
+                  </div>
+                )
+            })}
+        </div>
     )
-  } else {
-    return null
-  }
 }
