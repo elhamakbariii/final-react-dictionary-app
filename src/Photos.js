b3e5de86c767723be9523c9b41a76e0469718c6f
elhamakbariii <94735414+elhamakbariii@users.noreply.github.com>
2022-03-16 14:08:03 +0330

    file

--- src/Photos.js
+++ src/Photos.js
@@ -1,24 +1,28 @@
 import React from "react";
-import "./Photos.css"
+import "./Photos.css";
 
-export default function Photos(props){
-    if (props.photos){
+export default function Photos(props) {
+  if (props.photos) {
     return (
-        <section className="Photos">
-            <div className="row">
-            {props.photos.map(function(photo, index){
-                return (
-                    <div className="col-4 mb-3" key={index}>
-                    <a href={photo.src.original} target="_blank" rel="noreferrer">
-                    <img src={photo.src.landscape} className="img-fluid"/>
-                    </a>
-                    </div>
-                   );
-                })}
-            </div>  
-        </section>
+      <section className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt={photo.src.photographer}
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     );
-} else {
+  } else {
     return null;
-}
+  }
 }
