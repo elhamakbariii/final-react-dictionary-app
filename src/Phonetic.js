b9de9302a8b6365c113594f970a32ea5af00d14e
elhamakbariii <94735414+elhamakbariii@users.noreply.github.com>
2022-03-16 14:07:03 +0330

    file

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,25 +1,13 @@
 import React from "react";
-import "./Phonetic.css"
+import "./Phonetic.css";
 
-export default function Phonetic(props){
-    if (props.phonetic.audio)
-    {
-        return (
-            <div className="Phonetic">
-                <div className="audio">
-                    <audio  src={props.phonetic.audio} controls>
-                    </audio>
-                </div>
-                <div className="text">{props.phonetic.text}</div>
-            </div>
-        );
-    } else {
-        return (
-            <div className="audio">
-            <audio  src={props.phonetic.audio}>
-            </audio>
-            <div className="text">{props.phonetic.text}</div>
-            </div>
-        )
-    }
+export default function Phonetic(props) {
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        Listen
+      </a>
+      <span className="text">{props.phonetic.text}</span>
+    </div>
+  );
 }
