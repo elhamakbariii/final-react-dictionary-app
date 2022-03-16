39e2ddfe6b5b4c40473ac3b32190e1354db853b3
elhamakbariii <94735414+elhamakbariii@users.noreply.github.com>
2022-03-16 14:24:34 +0330

    file

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,25 @@
-import React from "react";
-import "./Phonetic.css";
+import React from 'react'
+import AudioPlayer from 'react-h5-audio-player'
+import 'react-h5-audio-player/lib/styles.css'
+import './Phonetic.css'
 
 export default function Phonetic(props) {
   return (
-    <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
-      <span className="text">{props.phonetic.text}</span>
+    <div className="Phonectics d-flex flex-row">
+      <AudioPlayer
+        src={props.phonetic.audio}
+        autoPlay={false}
+        customVolumeControls={[]}
+        customAdditionalControls={[]}
+        customProgressBarSection={[]}
+        style={{ width: '80px' }}
+        showJumpControls={false}
+        showFilledProgress={false}
+        showDownloadProgress={false}
+        showSkipControls={false}
+        layout="horizontal-reverse"
+      />
+      <span className="text"> {props.phonetic.text}</span>
     </div>
-  );
+  )
 }
